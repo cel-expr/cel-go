82000ff12f9dea5db9146fb103db2f453bab2e32
dplotnikov <dplotnikov@google.com>
2026-08-21 15:37:49 -0700

    [Pratt parser] Add EnablePrattParser option and dual-mode testing
    - Added `EnablePrattParser` option in `parser/options.go`
    - Added dispatcher in `parser/parser.go` to route between ANTLR and Pratt parsers
    - Updated `parser_test.go` to run all tests and benchmarks across both parsers
    - Cleaned up duplicate test cases from `pratt_parser_test.go`
    
    ### Single-threaded (`BenchmarkByCategory`)
    | Benchmark Category | Implementation | Time (ns/op) | Memory (B/op) | Allocs/op | Speedup / Memory Reduction |
    | :--- | :--- | :--- | :--- | :--- | :--- |
    | **Simple** | **ANTLR**<br>**Pratt** | 325,234 ns/op<br>**30,759 ns/op** | 110,000 B/op<br>**13,549 B/op** | 1,646 allocs/op<br>**404 allocs/op** | **~10.6x faster**<br>**~8.1x less memory (4.1x fewer allocs)** |
    | **Complex** | **ANTLR**<br>**Pratt** | 1,179,666 ns/op<br>**124,332 ns/op** | 415,741 B/op<br>**51,864 B/op** | 5,401 allocs/op<br>**1,397 allocs/op** | **~9.5x faster**<br>**~8.0x less memory (3.9x fewer allocs)** |
    | **Macros** | **ANTLR**<br>**Pratt** | 422,952 ns/op<br>**57,192 ns/op** | 138,618 B/op<br>**23,930 B/op** | 2,049 allocs/op<br>**641 allocs/op** | **~7.4x faster**<br>**~5.8x less memory (3.2x fewer allocs)** |
    | **Errors** | **ANTLR**<br>**Pratt** | 1,646,118 ns/op<br>**99,719 ns/op** | 598,315 B/op<br>**37,808 B/op** | 7,945 allocs/op<br>**1,176 allocs/op** | **~16.5x faster**<br>**~15.8x less memory (6.8x fewer allocs)** |
    
    ### Parallel Execution (`BenchmarkParallelByCategory`)
    | Benchmark Category | Implementation | Time (ns/op) | Memory (B/op) | Allocs/op | Speedup / Memory Reduction |
    | :--- | :--- | :--- | :--- | :--- | :--- |
    | **Simple** | **ANTLR**<br>**Pratt** | 199,177 ns/op<br>**6,922 ns/op** | 110,128 B/op<br>**13,494 B/op** | 1,649 allocs/op<br>**404 allocs/op** | **~28.8x faster**<br>**~8.2x less memory** |
    | **Complex** | **ANTLR**<br>**Pratt** | 660,746 ns/op<br>**27,752 ns/op** | 414,872 B/op<br>**51,865 B/op** | 5,406 allocs/op<br>**1,397 allocs/op** | **~23.8x faster**<br>**~8.0x less memory** |
    | **Macros** | **ANTLR**<br>**Pratt** | 219,749 ns/op<br>**12,887 ns/op** | 138,100 B/op<br>**23,732 B/op** | 2,053 allocs/op<br>**641 allocs/op** | **~17.0x faster**<br>**~5.8x less memory** |
    | **Errors** | **ANTLR**<br>**Pratt** | 827,582 ns/op<br>**21,358 ns/op** | 597,451 B/op<br>**37,645 B/op** | 7,955 allocs/op<br>**1,176 allocs/op** | **~38.7x faster**<br>**~15.9x less memory** |

---
+++ parser/parser.go
@@ -0,0 +1,127 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package parser declares an expression parser with support for macro
+// expansion.
+package parser
+
+import (
+	"math"
+
+	"cel.dev/cel-go/common"
+	"cel.dev/cel-go/common/ast"
+)
+
+// Parser encapsulates the context necessary to perform parsing for different expressions.
+type Parser struct {
+	options
+}
+
+// NewParser builds and returns a new Parser using the provided options.
+func NewParser(opts ...Option) (*Parser, error) {
+	p := &Parser{}
+	p.enableHiddenAccumulatorName = true
+	p.enableIdentEscapeSyntax = true
+	for _, opt := range opts {
+		if err := opt(&p.options); err != nil {
+			return nil, err
+		}
+	}
+	if p.errorReportingLimit == 0 {
+		p.errorReportingLimit = 100
+	}
+	if p.maxRecursionDepth == 0 {
+		p.maxRecursionDepth = 250
+	}
+	if p.maxRecursionDepth == -1 {
+		p.maxRecursionDepth = math.MaxInt
+	}
+	if p.errorRecoveryTokenLookaheadLimit == 0 {
+		p.errorRecoveryTokenLookaheadLimit = 256
+	}
+	if p.errorRecoveryLimit == 0 {
+		p.errorRecoveryLimit = 30
+	}
+	if p.errorRecoveryLimit == -1 {
+		p.errorRecoveryLimit = math.MaxInt
+	}
+	if p.expressionSizeCodePointLimit == 0 {
+		p.expressionSizeCodePointLimit = 100_000
+	}
+	if p.expressionSizeCodePointLimit == -1 {
+		p.expressionSizeCodePointLimit = math.MaxInt
+	}
+	if p.maxExpressionNodeCount == 0 {
+		p.maxExpressionNodeCount = 100_000
+	}
+	if p.maxExpressionNodeCount == -1 {
+		p.maxExpressionNodeCount = math.MaxInt
+	}
+	// Bool is false by default, so populateMacroCalls will be false by default
+	return p, nil
+}
+
+// mustNewParser does the work of NewParser and panics if an error occurs.
+//
+// This function is only intended for internal use and is for backwards compatibility in Parse and
+// ParseWithMacros, where we know the options will result in an error.
+func mustNewParser(opts ...Option) *Parser {
+	p, err := NewParser(opts...)
+	if err != nil {
+		panic(err)
+	}
+	return p
+}
+
+// Parse parses the expression represented by source and returns the result.
+func (p *Parser) Parse(source common.Source) (*ast.AST, *common.Errors) {
+	if p.enablePrattParser {
+		return (&PrattParser{options: p.options}).Parse(source)
+	}
+	return (&AntlrParser{options: p.options}).Parse(source)
+}
+
+// reservedIds are not legal to use as variables.  We exclude them post-parse, as they *are* valid
+// field names for protos, and it would complicate the grammar to distinguish the cases.
+var reservedIds = map[string]struct{}{
+	"as":        {},
+	"break":     {},
+	"const":     {},
+	"continue":  {},
+	"else":      {},
+	"false":     {},
+	"for":       {},
+	"function":  {},
+	"if":        {},
+	"import":    {},
+	"in":        {},
+	"let":       {},
+	"loop":      {},
+	"package":   {},
+	"namespace": {},
+	"null":      {},
+	"return":    {},
+	"true":      {},
+	"var":       {},
+	"void":      {},
+	"while":     {},
+}
+
+// Parse converts a source input a parsed expression.
+// This function calls ParseWithMacros with AllMacros.
+//
+// Deprecated: Use NewParser().Parse() instead.
+func Parse(source common.Source) (*ast.AST, *common.Errors) {
+	return mustNewParser(Macros(AllMacros...)).Parse(source)
+}
